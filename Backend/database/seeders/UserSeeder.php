03575f40c5a576096abe34b67e80bf9b61811685
Shine <120034529+Kimle7@users.noreply.github.com>
2024-08-08 15:41:38 +0700

    prepare userSeeder data for login

---
+++ Backend/database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Super Admin',
+            'email' => 'super_admin123@gmail.com',
+            'password'      => bcrypt('1234567890'),
+            'is_superadmin' => 1,
+        ]);
+    }
+}
