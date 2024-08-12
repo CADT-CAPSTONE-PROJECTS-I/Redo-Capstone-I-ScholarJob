9c2c5951787ee4c74ba19414db425facb898e5ed
Shine <120034529+Kimle7@users.noreply.github.com>
2024-08-12 14:47:50 +0700

    feature role

---
+++ Backend/resources/views/account/index.blade.php
@@ -0,0 +1,78 @@
+<x-app-layout>
+    <x-slot name="header">
+        {{ __('Accounts') }}
+        
+    </x-slot>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary card-outline">
+                <div class="card-header">
+                    <div class="card-tools">
+                        <a href="{{ route('accounts.create') }}" class="btn btn-primary" >
+                            <i class="fas fa-plus-circle"></i>
+                            {{__("Create Account")}}
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body"> 
+                    <form action="" id="form_filter">
+                        <div class="flex items-center">
+                            <div class=" md:w-1/2">
+                                <label class="block text-gray-700 font-medium mb-1">{{ __("Search") }}</label>
+                                <div class="flex">
+                                    <input type="text" name="search" placeholder="Search"
+                                        value="{{ request('search') }}"
+                                        class="form-input w-full rounded-l-md border-gray-300 focus:ring-indigo-500 focus:border-indigo-500">
+                                    <button type="submit" class="bg-gray-200 text-gray-700 hover:bg-gray-300 font-bold py-2 px-4 rounded-r-md">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-md-12">
+                                <button class="btn btn-info text-white" id="btn_filter">{{__("Filter")}} <i class="fas fa-filter"></i></button>
+                                <a href="{{ route('accounts.index') }}" class="btn btn-danger">
+                                    {{__('Clear')}}
+                                    <i class="fas fa-sync-alt"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+    
+                    <div class="table-responsive mt-3 px-2 py-2">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <th class="text-left text-uppercase bg-light">Name</th>
+                                <th class="text-left text-uppercase bg-light">Email</th>
+                                <th >Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td><a data-toggle="modal" data-target="#delete_user" class="btn btn-danger btn-sm" title="Delete"><i class="fa fa-trash"></i></a></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Search Form -->
+    {{-- <div class="align-items-center px-2 py-4 bg-white border mb-4 ">
+        <form method="GET" action="{{ route('accounts.index') }}" class="d-flex justify-content-between align-items-center">
+        <input type="text" id="success" class="col-md-4 ml-2 bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500" placeholder="Search">
+
+        </form>
+        <div class="mt-3">
+            <button type="submit" class="btn btn-primary btn-md ml-2">Filter</button>
+            <a href="{{ route('accounts.index') }}" class="btn btn-secondary btn-md ml-1 ">Clear</a>
+    </div> --}}
+
+</x-app-layout>
--- Backend/resources/views/users/index.blade.php
+++ Backend/resources/views/users/index.blade.php
@@ -3,49 +3,41 @@
         {{ __('Users') }}
     </x-slot>
 
-    <div class="mb-4 inline-flex overflow-hidden w-full bg-white rounded-lg shadow-md">
-        <div class="flex justify-center items-center w-12 bg-blue-500">
-            <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+    <div class="mb-4 d-inline-flex overflow-hidden w-100 bg-white rounded shadow">
+        <div class="d-flex justify-content-center align-items-center w-12 bg-primary">
+            <svg class="w-6 h-6 text-white" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
                 <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z"></path>
             </svg>
         </div>
 
-        <div class="px-4 py-2 -mx-3">
-            <div class="mx-3">
-                <span class="font-semibold text-blue-500">Info</span>
-                <p class="text-sm text-gray-600">Sample table page</p>
+        <div class="px-4 py-2">
+            <div>
+                <span class="font-weight-bold text-primary">Info</span>
+                <p class="text-muted">Sample table page</p>
             </div>
         </div>
     </div>
 
-    <div class="inline-block overflow-hidden min-w-full rounded-lg shadow">
-        <table class="min-w-full leading-normal">
+    <div class="table-responsive">
+        <table class="table table-bordered table-hover">
             <thead>
             <tr>
-                <th class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
-                    Name
-                </th>
-                <th class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
-                    Email
-                </th>
+                <th class="text-left text-uppercase bg-light">Name</th>
+                <th class="text-left text-uppercase bg-light">Email</th>
             </tr>
             </thead>
             <tbody>
             @foreach($users as $user)
                 <tr>
-                    <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                        <p class="text-gray-900 whitespace-no-wrap">{{ $user->name }}</p>
-                    </td>
-                    <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                        <p class="text-gray-900 whitespace-no-wrap">{{ $user->email }}</p>
-                    </td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
                 </tr>
             @endforeach
             </tbody>
         </table>
 
-        <div class="flex flex-col items-center px-5 py-5 bg-white border-t xs:flex-row xs:justify-between">
-            {{ $users->links() }}
+        <div class="d-flex justify-content-between align-items-center px-5 py-5 bg-white border-top">
+            {{ $users->links('pagination::bootstrap-4') }}
         </div>
     </div>
 
