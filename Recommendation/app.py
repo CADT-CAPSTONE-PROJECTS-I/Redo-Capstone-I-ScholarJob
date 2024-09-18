6c7afdefd1455fd38734537cb2755a45a45aa889
JinOhara <darachin.kong@student.cadt.edu.kh>
2024-09-18 14:33:56 +0700

    add recommendation folder

---
+++ Recommendation/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from collaborative import cf_recommend
+from content_based import cbf_recommend
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Welcome to the Job Recommendation System!'
+
+# Register routes
+app.add_url_rule('/collaborative', view_func=cf_recommend, methods=['POST'])
+app.add_url_rule('/content_based', view_func=cbf_recommend, methods=['POST'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
